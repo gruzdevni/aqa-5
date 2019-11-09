76d0cf41aeb3f24f05a123de4a7e02f9ff7fe7c4
Artem Romanov <romanov-artyom@bk.ru>
2019-11-09 12:22:47 +0300

    Перенес методы с faker в класс генерации данных

--- src/test/java/DataGeneration.java
+++ src/test/java/DataGeneration.java
@@ -7,6 +7,7 @@ import java.util.Locale;
 import java.util.Random;
 
 public class DataGeneration {
+    private static Faker faker = new Faker(new Locale("ru"));
 
     public static String getCity() {
         String[] cities = {"Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала", "Магас", "Нальчик", "Элиста", "Черкесск", "Петрозаводск", "Сыктывкар", "Симферополь", "Йошкар-Ола", "Саранск", "Якутск", "Владикавказ", "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный", "Чебоксары", "Барнаул", "Чита", "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь", "Владивосток", "Ставрополь", "Хабаровск", "Благовещенск", "Архангельск", "Астрахань", "Белгород", "Брянск", "Владимир", "Волгоград", "Вологда", "Воронеж", "Иваново", "Иркутск", "Калининград", "Калуга", "Кемерово", "Киров", "Кострома", "Курган", "Курск", "Санкт-Петербург", "Липецк", "Магадан", "Москва", "Мурманск", "Нижний Новгород", "Великий Новгород", "Новосибирск", "Омск", "Оренбург", "Орёл", "Пенза", "Псков", "Ростов-на-Дону", "Рязань", "Самара", "Саратов", "Южно-Сахалинск", "Екатеринбург", "Смоленск", "Тамбов", "Тверь", "Томск", "Тула", "Тюмень", "Ульяновск", "Челябинск", "Ярославль", "Москва", "Санкт-Петербург", "Севастополь", "Биробиджан", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь", "Салехард"};
@@ -20,5 +21,17 @@ public class DataGeneration {
         DateTimeFormatter formatPattern = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String firstAvailableDateFormatted = formatPattern.format(firstAvailableDate);
         return firstAvailableDateFormatted;
+    }
+
+    public static String getName() {
+        String lastName = faker.name().lastName();
+        String firstName = faker.name().firstName();
+        String name = lastName + " " + firstName;
+        return name;
+    }
+
+    public static String getPhone() {
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
     }
 }
